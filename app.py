863015575f951dec6183b528a2d9f756ebb46b95
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-07-20 18:08:22 -0700

    logic to prepare avg visits data for pie chart

--- app.py
+++ app.py
@@ -81,6 +81,22 @@ for ph in passholder_visits_by_year:
   else:
     passholder_visits_by_year[ph]['avg_visits'] = 0
 
+avg_visits = []
+for ph in passholder_visits_by_year:
+  avg_visits.append(passholder_visits_by_year[ph]['avg_visits']
+
+avg_visits_grouped = {'less than 1': 0, '1-2': 0, '2-3': 0, '3-5': 0, 'More than 5': 0}
+for num in avg_visits:
+  if num < 1:
+    avg_visits_grouped['less than 1'] += 1
+  elif 1 <= num < 2:
+    avg_visits_grouped['1-2'] += 1
+  elif 2 <= num < 3:
+    avg_visits_grouped['2-3'] += 1
+  elif 3 <= num <= 5:
+    avg_visits_grouped['3-5'] += 1
+  elif 5 < num:
+    avg_visits_grouped['More than 5'] += 1
 
 app.layout = html.Div(children=[
   html.H1("Hi", id="title"),
